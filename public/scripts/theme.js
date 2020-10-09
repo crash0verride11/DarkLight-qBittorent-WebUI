34b66392176c402a75107388b77e816b2f382c6f
Raymond Lanser <raylanser@me.com>
2020-10-09 18:39:49 -0400

    Forget Toggled Theme

--- public/scripts/theme.js
+++ public/scripts/theme.js
@@ -2,20 +2,28 @@
 
 // Select the theme preference from localStorage
 const currentTheme = localStorage.getItem("theme");
+const prevDate = new Date(localStorage.getItem("themeDate"));	
+let curDate = new Date();
+// If themeForget is true the theme value will be ignored after a set number of hours
+let themeForget = true;
+// How many hours before the theme value is ignored
+var themeClock = 3;
+// Difference between time of last set theme preference value and current time.
+var dateDif = parseInt((curDate-prevDate)/(3600*1000));
 // If the current theme in localStorage is "dark"...
 function themeLoad() {
-if (currentTheme == "dark") {
-  // ...then use the .dark-theme class
-	var test = window.matchMedia('(prefers-color-scheme: dark)').matches;
-	if (window.matchMedia('(prefers-color-scheme: dark)').matches == false) {
-		document.body.classList.toggle("dark-theme");
+	if ((dateDif <= themeClock) || themeForget == false){
+		if (currentTheme == "dark") {
+			// ...then use the .dark-theme class
+			if (window.matchMedia('(prefers-color-scheme: dark)').matches == false) {
+				document.body.classList.toggle("dark-theme");}
+		}
+		if (currentTheme == "light") {
+			// ...then use the .dark-theme class
+			if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+				document.body.classList.toggle("light-theme");}
+		}	
 	}
-}
-if (currentTheme == "light") {
-  // ...then use the .dark-theme class
-	if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-		document.body.classList.toggle("light-theme");}
-	}	
 }
 // Listen for a click on the button 
 function themeSwap() {
@@ -30,8 +38,7 @@ function themeSwap() {
 		// If the body .light-theme class was toggled on, correct the theme variable.
 		if (document.body.classList.contains("light-theme")) {
 			// ...then let's make the theme dark
-			theme = "light";
-		}
+			theme = "light";}
 		// Then save the choice in localStorage
 		localStorage.setItem("theme", theme);
 	} else {
@@ -44,8 +51,7 @@ function themeSwap() {
 		// If the body .dark-theme class was toggled on, correct the theme variable.
 		if (document.body.classList.contains("dark-theme")) {
 			// ...then let's make the theme dark
-			theme = "dark";
-		}
+			theme = "dark";}
 		// Then save the choice in localStorage
 		localStorage.setItem("theme", theme);}
 }
